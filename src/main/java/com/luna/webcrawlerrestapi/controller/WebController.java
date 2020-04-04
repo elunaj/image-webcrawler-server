318aa1a536b6ce8e72c64c76f6ac692141b4d8db
elunaj <elias.luna@mail.citytech.cuny.edu>
2020-04-03 22:29:08 -0400

    cors debugging

--- src/main/java/com/luna/webcrawlerrestapi/controller/WebController.java
+++ src/main/java/com/luna/webcrawlerrestapi/controller/WebController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 
-@CrossOrigin(origins = {"http://localhost:3000/"}, exposedHeaders = {"Content-Disposition"})
+@CrossOrigin(origins = {"*"}, exposedHeaders = {"Content-Disposition"})
 @RestController
 public class WebController {
 
